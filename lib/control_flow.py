03e819cdd67fb400498c2f4ad96e0e417e72509e
Eric Krukowski <erickruk@amazon.com>
2023-07-03 01:53:52 -0400

    Lab complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,43 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    access_statement = "Access granted" if (username=="admin" or username=="ADMIN") and password=="12345" else "Access denied"
+    return access_statement
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if(temperature<40):
+        return "It's brisk out there!"
+    elif(temperature<65):
+        return "It's a little chilly out there!"
+    elif(temperature>85):
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     # your code here
-    pass
-
+    if (num%3==0):
+        if(num%5==0):
+            return "FizzBuzz"
+        else:
+            return "Fizz"
+    else:
+        if(num%5==0):
+            return "Buzz"
+        else:
+            return num
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if (operation=="+"):
+        return num1+num2
+    elif(operation == "-"):
+        return num1-num2
+    elif(operation == "*"):
+        return num1*num2
+    elif(operation=="/"):
+        return num1/num2
+    else:
+        print("Invalid operation!")
+        return None
